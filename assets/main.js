efb999fb5e450ddd172ecce5a4d56fad40498e94
leusio jose filimone <leusiojosefilimone@gmail.com>
2022-11-21 10:22:46 -0500

    first commit

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector(".menu-mobile")
+let open = document.querySelector("#menu-button-open")
+let close = document.querySelector("#menu-button-close")
+
+function slideMenu(){
+    let body = document.querySelector("body")
+    body.classList.toggle("menu-slided")
+
+}
+
