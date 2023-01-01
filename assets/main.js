a826d09dcf247a62874e46ab98b21e24547ad002
leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-01-01 16:43:20 +0200

    dinamizando publicações

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector(".menu-mobile")
 let open = document.querySelector("#menu-button-open")
 let close = document.querySelector("#menu-button-close")
+const container = document.querySelector('.publications')
 
 function slideMenu(){
     let body = document.querySelector("body")
@@ -8,3 +9,33 @@ function slideMenu(){
 
 }
 
+const publication = [
+    {
+       imagem: "assets/images/image-retro-pcs.jpg",
+          id: 01,
+          titulo:' Reviving Retro PCs',
+          coments:' What happens when old PCs are given modern upgrades?'
+    },
+    {
+        imagem: "assets/images/image-top-laptops.jpg",
+           id: 02,
+           titulo:'Top 10 Laptops of 2022',
+           coments:'Our best picks for various needs and budgets.'
+     },
+     {
+        imagem: "assets/images/image-gaming-growth.jpg",
+           id: 03,
+           titulo:'The Growth of Gaming ',
+           coments:'How the pandemic has sparked fresh opportunities.'
+     }
+]
+publication.map((publicacao) => {
+container.innerHTML += ` <article>
+<img src="${publicacao.imagem}" alt="">
+<span>
+<h1>${publicacao.id}</h1>
+<h3>${publicacao.titulo}</h3>
+<p>${publicacao.coments}</p>
+</span>
+</article>`
+} )
